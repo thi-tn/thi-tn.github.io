96bfc947268bf8867ab84c482bec726445032f46
Thi Nguyen <katnguyen2612@gmail.com>
2017-11-26 11:48:57 -0500

    Repo Created

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+	$('.works-thumb').click(function() {
+		var id = $('.works-thumb').index($(this));
+		$('#works .carousel-item.active').removeClass('active');
+		$('#works .carousel-item').eq(id).addClass('active');
+		// var carouselSrc = $(this).attr('src');
+		// var carouselAlt = $(this).attr('alt');
+		// $('#works .carousel-item img').attr('src', carouselSrc).attr('alt', carouselAlt);
+		// $('#works .carousel-item img').eq(id - 1).attr('src', prev.attr('src')).attr('alt', prev.attr('alt'));
+		// $('#works .carousel-item img').eq(id + 1).attr('src', next.attr('src')).attr('alt', next.attr('alt'));
+	});
+});
