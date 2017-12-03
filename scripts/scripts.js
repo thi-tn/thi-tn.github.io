cc5d7b6982bf54a2f597d7ab6dc196a39c2deccb
Thi Nguyen <thi.nguyen261296@gmail.com>
2017-12-02 19:11:46 -0500

    Design Revision

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,4 +9,7 @@ $(function() {
 		// $('#works .carousel-item img').eq(id - 1).attr('src', prev.attr('src')).attr('alt', prev.attr('alt'));
 		// $('#works .carousel-item img').eq(id + 1).attr('src', next.attr('src')).attr('alt', next.attr('alt'));
 	});
+	$('nav .navbar-toggler').click(function() {
+		$('nav').toggleClass('activeButton');
+	});
 });
